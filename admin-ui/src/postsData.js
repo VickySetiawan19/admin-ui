22886fccb07bc0fe716a2a44ecc41609830d4620
VickySetiawan19 <setiawangans04@gmail.com>
2026-04-28 13:14:18 +0700

    uts - postcard

--- admin-ui/src/postsData.js
+++ admin-ui/src/postsData.js
@@ -1,24 +1,4 @@
-const city = "Bandung";
-const street = "JL. Ahmad Yani";
-
-export const getUsers = async () => {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await response.json();
-    return users.map((user) => ({
-      name: user.name,
-      email: user.email,
-      city,
-      street,
-    }));
-  } catch (error) {
-    console.error("[Services] Gagal mengambil data:", error.message);
-    throw error;
-  }
-};
-
-
-export const getPosts = [
+[
   {
     "userId": 1,
     "id": 1,
