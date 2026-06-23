a54c21cd7cb7160b7ab6166257408dd7bcd20302
VickySetiawan19 <setiawangans04@gmail.com>
2026-06-23 14:01:33 +0700

    State Management with Context

--- admin-ui/src/main.jsx
+++ admin-ui/src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-// Hapus: import Exercise from './latihan/Exercise.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Exercise from "../latihan/Exercise.jsx";
+import Parent from "./latihan/Parent.jsx";
+import { CounterContextProvider } from "./context/counterContext.jsx";
+import { ThemeContextProvider } from "./context/themeContext.jsx";
+ 
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeContextProvider>
+      <App/>
+    </ThemeContextProvider>
+  </React.StrictMode>,
 );
